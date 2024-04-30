fd3be1fdc55e9cd09d8a616164ae7af8a740b616
francisoodde <168544748+francisoodde@users.noreply.github.com>
2024-04-30 13:11:25 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "09044857067"],
+global.ownernumber = [ process.env.OWNER_NUMBER || "08069363347"],
 global.ownername = process.env.OWNER_NAME || Francis,
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
